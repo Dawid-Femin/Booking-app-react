94d80f5d2ae24247986eb8f7bad4467e226755cc
Dawid Femin <kontakt@dawidfemin.pl>
2022-07-06 15:20:07 +0200

    Node MongoDB - created Login and Register

--- api/index.js
+++ api/index.js
@@ -22,6 +22,7 @@ mongoose.connection.on('disconnected', () => {
     console.log('MongoDB disconected');
 });
 
+//middkewares
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
@@ -29,6 +30,17 @@ app.use('/api/users', usersRoute);
 app.use('/api/hotels', hotelsRoute);
 app.use('/api/rooms', roomsRoute);
     
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong!";
+    return res.status(errorStatus).json({
+      success: false,
+      status: errorStatus,
+      message: errorMessage,
+      stack: err.stack,
+    });
+  });
+
 app.listen(8800, () => {
     connect();
     console.log('Connected to backend!');
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,13 +1,9 @@
-import express from 'express';
+import express from "express";
+import { register, login } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello from auth endpoint')
-});
-
-router.get('/register', (req, res) => {
-    res.send('Hello from auth register endpoint')
-});
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
